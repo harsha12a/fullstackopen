50f0b226c7e03f0553ebb0b2b1401e9a5d39b813
Sree Satya Harsha <harsha.pss2005@gmail.com>
2024-09-22 15:17:02 +0530

    part1 1.5

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -2,26 +2,28 @@ import Content from "./components/Content";
 import Header from "./components/Header";
 import Total from "./components/Total";
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
   return (
     <div>
-      <Header course={course}/>
-      <Content course={parts} />
-      <Total course={parts} />
+      <Header course={course.name}/>
+      <Content course={course.parts} />
+      <Total course={course.parts} />
     </div>
   );
 };
