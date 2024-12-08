1247bd9f0abd2eca6a965248ae77e76cbd8cb605
harsha12a <harsha.pss2005@gmail.com>
2024-12-08 21:09:36 +0530

    part2 2.18

---
+++ part2/countries/src/App.jsx
@@ -0,0 +1,57 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+function App() {
+  let [filter, setFilter] = useState("");
+  let [countries, setCountries] = useState([]);
+  useEffect(() => {
+    axios
+      .get("https://studies.cs.helsinki.fi/restcountries/api/all")
+      .then((res) => {
+        setCountries(res.data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  let filteredCountries = countries.filter((country) => {
+    return country.name.common.toLowerCase().includes(filter.toLowerCase());
+  });
+  let handleChange = (e) => setFilter(e.target.value);
+  return (
+    <div>
+      <form action="">
+        <label htmlFor="">find countries</label>
+        <input type="text" onChange={handleChange} />
+      </form>
+      {filter === "" ? (
+        <p>Please enter a query</p>
+      ) : filteredCountries.length > 10 ? (
+        <p>Too many matches, specify another filter</p>
+      ) : filteredCountries.length === 1 ? (
+        <div>
+          <h1>{filteredCountries[0].name.common}</h1>
+          <div>capital {filteredCountries[0].capital}</div>
+          <div>area {filteredCountries[0].area}</div>
+          <div style={{ margin: "20px 0", fontWeight: "bold" }}>languages:</div>
+          {filteredCountries[0].languages && (
+            <ul>
+              {Object.values(filteredCountries[0].languages).map((language) => (
+                <li key={language}>{language}</li>
+              ))}
+            </ul>
+          )}
+          <img
+            src={filteredCountries[0].flags.png}
+            alt={filteredCountries[0].name.common}
+            width={"150px"}
+          />
+        </div>
+      ) : (
+        filteredCountries.map((country) => {
+          return <div key={country.name.common}>{country.name.common}</div>;
+        })
+      )}
+    </div>
+  );
+}
+
+export default App;
