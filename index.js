d4b9839f5c213ababd8b4ce36af3d1f150514211
harsha12a <harsha.pss2005@gmail.com>
2025-01-11 12:33:29 +0530

    part3 3.8

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const app = express();
 app.use(express.json());
 app.use(morgan('tiny'));
 
+morgan.token('body', (req, res) => JSON.stringify(req.body));
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
+
 let users = [
   {
     id: 1,
