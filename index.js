628cc70830ec8d0e3c7bfdee6c57194586e24df8
harsha12a <harsha.pss2005@gmail.com>
2025-01-11 12:37:06 +0530

    part3 3.8 final

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require("express");
 const morgan = require('morgan');
 const app = express();
 app.use(express.json());
-app.use(morgan('tiny'));
 
 morgan.token('body', (req, res) => JSON.stringify(req.body));
 
