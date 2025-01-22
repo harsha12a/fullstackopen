fc963823dea48b4d2e422028548d8f9862663050
harsha12a <harsha.pss2005@gmail.com>
2025-01-22 19:48:40 +0530

    part3 3.13

--- part3/phonebookBackend/index.js
+++ part3/phonebookBackend/index.js
@@ -1,35 +1,18 @@
 const express = require("express");
 const morgan = require('morgan');
 const cors = require('cors');
+require("dotenv").config();
+const Person = require('./models/person');
 const app = express();
+
+
 app.use(express.json());
 app.use(cors())
 morgan.token('body', (req, res) => JSON.stringify(req.body));
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 
-let users = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
+let users = []
 
 app.get("/", (req, res) => {
   res.send("<h1>Welcome to Phonebook Backend</h1>");
@@ -78,10 +61,12 @@ app.post('/api/persons', (req, res) => {
 })
 
 app.get("/api/persons", (req, res) => {
-  res.json(users);
+  Person.find({}).then((persons) => {
+    res.json(persons);
+  });
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
   console.log("Server Running");
---
+++ part3/phonebookBackend/models/person.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose')
+
+mongoose.set('strictQuery', false)
+
+const url = process.env.MONGODB_URI
+
+mongoose.connect(url)
+    .then(() => console.log("Connected to mongo"))
+    .catch((err) => console.log("Error connecting to mongo",err.message))
+
+let personSchema = new mongoose.Schema({
+    name: String,
+    number: String
+})
+
+personSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+    }
+})
+
+module.exports = mongoose.model('Person', personSchema)
